61fbf3bab92c6a86f034374cae790aee78744ed5
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-08 23:16:34 -0300

    exer072_tupla
    Exercício utilizando tupla.

---
+++ exer072_tupla.py
@@ -0,0 +1,8 @@
+# o programa pedirá um numero ao usuario entre 1 e 10
+# e o exibirá por extenso esse número.
+
+nr=('0','um','dois','tres','quatro','cinco','seis','sete','oito','nove','dez')
+pos=int(input('Digite um número entre 1 e 10_'))
+
+print('VC digitou ',nr[pos])
+
