ece9260ba84fecf42e2acc806400fcbb2714d92a
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-09 19:18:46 -0300

    adição_filtro_linha
    Adição de filtro de linha para digitação correta por parte do usuário.

--- exer072_tupla.py
+++ exer072_tupla.py
@@ -2,7 +2,11 @@
 # e o exibirá por extenso esse número.
 
 nr=('0','um','dois','tres','quatro','cinco','seis','sete','oito','nove','dez')
-pos=int(input('Digite um número entre 1 e 10_'))
+
+while True: # linha para garantir digitação correta pelo usuario
+    pos=int(input('Digite um número entre 1 e 10_'))
+    if 1<=pos<=10:
+        break
 
 print('VC digitou ',nr[pos])
 
