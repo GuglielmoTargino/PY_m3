3b81572e26f55cf6e4e16fe654f5ad5378719c67
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-13 19:56:17 -0300

    exer074_randon_tupla
    Exercício utilizando numero aleatório em tuplas.

---
+++ exer074_randon_tupla.py
@@ -0,0 +1,7 @@
+# programa para armazenar numeors aleatorios em tuplas.
+from random import randint
+nr=(randint(1,100),randint(1,50),randint(1,60),randint(1,40),randint(1,30),randint(1,16),randint(1,68))
+print(f"{nr}")
+print(f" o maio numero é {max(nr)}")
+print(f"o menor numero é {min(nr)}")
+
