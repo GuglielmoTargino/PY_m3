66c70e06cfffdab69adb760ad829dd299903d997
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-15 22:39:10 -0300

    adição de comentário
    Adição de comentários na linha 4 apenas .

--- exer074_randon_tupla.py
+++ exer074_randon_tupla.py
@@ -1,7 +1,7 @@
 # programa para armazenar numeors aleatorios em tuplas.
 from random import randint
 nr=(randint(1,100),randint(1,50),randint(1,60),randint(1,40),randint(1,30),randint(1,16),randint(1,68))
-print(f"{nr}")
+print(f"{nr}")# linha para imprimir a tupla por extenso.
 print(f" o maio numero é {max(nr)}")
 print(f"o menor numero é {min(nr)}")
 
