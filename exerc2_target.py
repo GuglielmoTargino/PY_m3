570d050f38f69b7ff18a2f23b65439b8654ce4d5
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-27 15:54:00 -0300

    exerc2_target
    Exercício feito em teste de estágio (Target).

---
+++ exerc2_target.py
@@ -0,0 +1,24 @@
+#programa para verificar se um numero informado pelo usuário 
+#pertence a sequencia de Fibonacci.
+nr=int(input("Informe um número inteiro para saber se ele pertence à sequencia de Fibonacci_"))
+
+ref=nr #variável <ref> criada para receber o número digitado pelo usário e limitar a sequência Fibonacci.
+nr0=0 # <nr0> é o primeiro numero da sequência
+nr1=1 # <nr1> é o segundo número da sequência.
+nr3=0 # variável criada para receber o último número da sequência. Começa zerada.
+
+while nr3<=ref
+
+
+
+
+
+
+
+
+
+
+print(f"{ref}")
+
+
+
