029e78d3587a788d7679acf20e0e662f1bd46e84
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-29 21:58:58 -0300

    aula_eng_software_lista
    Finalização do exercício da aula com string(lista ).

--- exerc_aula_eng_software.py
+++ exerc_aula_eng_software.py
@@ -1,7 +1,47 @@
 
-soma1=10+15
-soma2=25+15
-soma3=5+25
-soma4=2+35
-print(f" as quatro somas são {soma1},{soma2},{soma3},{soma4}")
+#soma1=10+15
+#soma2=25+15
+#soma3=5+25
+#soma4=2+35
+#print(f" as quatro somas são {soma1},{soma2},{soma3},{soma4}")
+
+#def soma(numero1, numero2):#cria uma função para se somar dois numeros.
+  #return numero1 + numero2 # a variável <soma> returna com o valor de return.
+
+#print(f"A soma do número 5 e 6 é de {soma(5,6)}")# (5,6) o 5=numero1, e o 6=numero2.
+
+def saudacao(nome):
+    print(f" Bem vindo {nome}")
+
+saudacao("guga")
+
+#================================================
+def calcular_quadrado(numero):
+  return numero * numero
+
+def calcular_soma(numero1, numero2):
+  soma = numero1 + numero2
+  print(f'A soma dos números {numero1} e {numero2} é de {soma}')
+  #=============================================================
+
+  # Função principal
+def main(): # abre com main e tem que fechar lá embaixo
+  # Lê dois número dado pelo usuário
+  numero1 = float(input("Digite o primeiro número:\t")) 
+  numero2 = float(input("Digite o segundo número:\t"))
+
+  # Calcula o quadrado do primeiro número
+  quadrado = calcular_quadrado(numero1)
+
+  # Calcula soma dos dois números informados
+  calcular_soma(numero1, numero2)
+
+  # Imprime a soma dos dois números
+  print('O quadrado de ', numero1, " é ", quadrado)
+main() # fecha o bloco com main.
+
+
+    
+
+
 
---
+++ exerc_engsoftware_lista.py
@@ -0,0 +1,11 @@
+lista1=[2,3,4,8]
+lista=[8,6,4]
+soma=0
+for item in lista1:
+    soma=soma+item
+    print(soma)
+
+print(len(lista))
+
+
+
