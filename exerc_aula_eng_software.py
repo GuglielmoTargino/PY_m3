2c76406a71657483a06e3e0536db407e64377012
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-03-30 12:55:48 -0300

    finalizado a o exerc. fun
    Finalização do exercício de função com return.

--- exerc_aula_eng_software.py
+++ exerc_aula_eng_software.py
@@ -1,37 +1,30 @@
 
-#soma1=10+15
-#soma2=25+15
-#soma3=5+25
-#soma4=2+35
-#print(f" as quatro somas são {soma1},{soma2},{soma3},{soma4}")
+  
+def calcular_quadrado(q):
+  return q*q
 
-#def soma(numero1, numero2):#cria uma função para se somar dois numeros.
-  #return numero1 + numero2 # a variável <soma> returna com o valor de return.
-
-#print(f"A soma do número 5 e 6 é de {soma(5,6)}")# (5,6) o 5=numero1, e o 6=numero2.
-
-def saudacao(nome):
-    print(f" Bem vindo {nome}")
-
-saudacao("guga")
+def calcular_soma(numero1,numero2):
+  return numero2+numero1 
 
 
-
-  # Função principal
+# Função principal
 def main(): # abre com main e tem que fechar lá embaixo
   # Lê dois número dado pelo usuário
   numero1 = float(input("Digite o primeiro número:\t")) 
   numero2 = float(input("Digite o segundo número:\t"))
 
   # Calcula o quadrado do primeiro número
-  quadrado = calcular_quadrado(numero1)
+  quadrado = calcular_quadrado(numero1)  
 
   # Calcula soma dos dois números informados
-  calcular_soma(numero1, numero2)
-
-  # Imprime a soma dos dois números
+  soma=calcular_soma(numero1, numero2)
+  
+   # Imprime a soma dos dois números
   print('O quadrado de ', numero1, " é ", quadrado)
+  print(f"a soma de {numero1} com {numero2} é {soma}")
+
 main() # fecha o bloco com main.
+
 
 
     
