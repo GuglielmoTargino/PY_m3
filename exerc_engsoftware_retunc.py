4e61c41dc4381e086d4c728689d5ca41d04789e8
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-04-12 21:42:26 -0300

    treino_valores_
    treino trocando valores com a biblioteca time.

--- exerc_engsoftware_retunc.py
+++ exerc_engsoftware_retunc.py
@@ -1,4 +1,6 @@
 # Criando um decorador sem notação
+import time # importe da biblioteca time
+
 def criar_potencia(x): #x^y
   def potencia(y):
     return x ** y
@@ -7,3 +9,7 @@ def criar_potencia(x): #x^y
 # Potência de 5 elevado a 10
 resultado = criar_potencia(3)
 print(resultado(2))
+#imorimindo a data
+print(time.localtime()[2])
+print(time.localtime()[1])
+print(time.localtime()[0])
