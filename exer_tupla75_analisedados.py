011c74f571e5b37fcfee5be432a7dbc16de06285
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-04-13 16:58:21 -0300

    treino_apenas
    troca de valores apenas

--- exer_tupla75_analisedados.py
+++ exer_tupla75_analisedados.py
@@ -1,2 +1,5 @@
-num=int(input('digite um numero_'))
-print(f'vc digitou {num}')
+num=(0,int(input('digite um numero_')),int(input('digiite outro numero_')),int(input('digite mais um numero')))
+
+print(f'vc digitou {num[1]}')
+print(f'o numero 2 apareceu {num.count(2)} vezes')
+print(f' o numero 5 está na posiçao {num.index(5)}º')
