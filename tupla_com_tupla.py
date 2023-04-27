d725a086e849925bd3bb47aeb09d4fd4bdf92e11
Guglielmo Henriques Targino <guglielmohenriquestargino@gmail.com>
2023-04-26 22:32:33 -0300

    exer_tupla
    Exercício com tupla dentro de tupla

---
+++ tupla_com_tupla.py
@@ -0,0 +1,9 @@
+#programa com tupla dentro de tupla.
+palavra=('carro','livro','pedra')
+
+print(f" a lista é {palavra}")
+for g in palavra:
+    if g in 'aeiou'
+    print(f"cada palavra é {g}")
+    
+
